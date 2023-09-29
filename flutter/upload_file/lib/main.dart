0e1a54382fcab277eb7326dd29adcf5d95feade0
sota1111 <sota.moro@gmail.com>
2023-09-29 16:34:07 +0900

    リファクタリング

--- flutter/upload_file/lib/main.dart
+++ flutter/upload_file/lib/main.dart
@@ -1,34 +1,36 @@
 import 'dart:convert';
-import 'dart:typed_data';  // Added for Uint8List
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(FileUploaderApp());
 }
 
-class MyApp extends StatelessWidget {
+class FileUploaderApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: FileUploaderScreen(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class FileUploaderScreen extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _FileUploaderScreenState createState() => _FileUploaderScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  Uint8List? _fileBytes;  // To store the file bytes
-  String? _fileName;  // To store the file name
-  int? _statusCode;
-  String _responseText = "No response yet";
+class _FileUploaderScreenState extends State<FileUploaderScreen> {
+  Uint8List? _selectedFileBytes;
+  String? _selectedFileName;
+  int? _uploadStatusCode;
+  String _uploadResponse = "";
+  String _serverResponse = "";
 
-  void _chooseFile() async {
+  // Pick a CSV file
+  void _pickCsvFile() async {
     final result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['csv'],
@@ -36,58 +38,75 @@ class _HomeScreenState extends State<HomeScreen> {
 
     if (result != null) {
       setState(() {
-        _fileBytes = result.files.first.bytes;
-        _fileName = result.files.first.name;
+        _selectedFileBytes = result.files.first.bytes;
+        _selectedFileName = result.files.first.name;
       });
     }
   }
 
-  void _uploadFile() async {
-    if (_fileBytes != null && _fileName != null) {
-      final base64Data = base64Encode(_fileBytes!);
-
-      final response = await http.post(
-        Uri.parse('https://3gxeogvzp2.execute-api.ap-northeast-1.amazonaws.com/Prod/upload'),
-        body: jsonEncode({
-          'file_name': _fileName,
-          'file_data': base64Data,
-        }),
-        headers: {"Content-Type": "application/json"},
-      );
-
+  // Send the file to the server
+  void _sendFileToServer() async {
+    if (_selectedFileBytes == null || _selectedFileName == null) {
       setState(() {
-        _statusCode = response.statusCode;
+        _uploadResponse = "No file selected";
       });
-      var responseBody = jsonDecode(response.body);
-      var serverMessage = responseBody['message'];
+      print("No file selected");
+      return;
+    }
 
-      print("Server Message: $serverMessage");  // Print server message
-      print("Status Code: $_statusCode");  // Print status code
+    final base64FileData = base64Encode(_selectedFileBytes!);
+    final response = await _performFileUpload(base64FileData);
 
-      if (response.statusCode == 200) {
-        print("File uploaded successfully");
-      } else {
-        print("Failed to upload file");
-      }
+    _updateUploadStatus(response);
+  }
+
+  // Perform the actual file upload
+  Future<http.Response> _performFileUpload(String base64FileData) {
+    return http.post(
+      Uri.parse('https://3gxeogvzp2.execute-api.ap-northeast-1.amazonaws.com/Prod/upload'),
+      body: jsonEncode({
+        'file_name': _selectedFileName,
+        'file_data': base64FileData,
+      }),
+      headers: {"Content-Type": "application/json"},
+    );
+  }
+
+  // Update the upload status
+  void _updateUploadStatus(http.Response response) {
+    setState(() {
+      _uploadStatusCode = response.statusCode;
+    });
+
+    var responseBody = jsonDecode(response.body);
+    _uploadResponse = responseBody['message'];
+    print("Server Message: $_uploadResponse");
+    print("Status Code: $_uploadStatusCode");
+
+    if (response.statusCode == 200) {
+      print("File uploaded successfully");
+    } else {
+      print("Failed to upload file");
     }
   }
 
-  Future<void> _getData() async {
+  // Fetch data from the server
+  Future<void> _fetchServerData() async {
     try {
       final response = await http.get(Uri.parse('https://3gxeogvzp2.execute-api.ap-northeast-1.amazonaws.com/Prod/hello'));
 
       if (response.statusCode == 200) {
         setState(() {
-          _responseText = json.decode(response.body)['message'];
+          _serverResponse = json.decode(response.body)['message'];
         });
       } else {
         setState(() {
-          _responseText = 'Failed to get data';
+          _serverResponse = 'Failed to get data';
         });
       }
     } catch (e) {
       setState(() {
-        _responseText = 'Exception occurred: $e';
+        _serverResponse = 'Exception occurred: $e';
       });
     }
   }
@@ -95,29 +114,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("File Upload")),
+      appBar: AppBar(title: const Text("File Upload")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: _chooseFile,
-              child: Text("Choose File"),
+              onPressed: _pickCsvFile,
+              child: const Text("Choose File"),
             ),
-            SizedBox(height: 10),
-            Text("Selected File: $_fileName"), // Show selected file name
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
+            Text("Selected File: $_selectedFileName"),
+            const SizedBox(height: 50),
             ElevatedButton(
-              onPressed: _uploadFile,
-              child: Text("Upload"),
+              onPressed: _sendFileToServer,
+              child: const Text("Upload"),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 10),
+            Text("Server Massage: $_uploadResponse"),
+            const SizedBox(height: 50),
             ElevatedButton(
-              onPressed: _getData,
-              child: Text("GET Data"),
+              onPressed: _fetchServerData,
+              child: const Text("GET Data"),
             ),
-            SizedBox(height: 20),
-            Text("Response: $_responseText"),
+            const SizedBox(height: 10),
+            Text("Response: $_serverResponse"),
           ],
         ),
       ),
