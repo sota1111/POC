2f5360a0ef3c8d5fb879949399b150e9aadbe12b
sota1111 <sota.moro@gmail.com>
2023-10-02 13:12:28 +0900

    カレンダー表示

--- flutter/upload_file/lib/main.dart
+++ flutter/upload_file/lib/main.dart
@@ -3,50 +3,61 @@ import 'upload_view.dart';
 import 'download_view.dart';
 
 void main() {
-  runApp(const PlotDataApp());
+  runApp(PlotDataApp());
 }
 
-class PlotDataApp extends StatefulWidget {
-  const PlotDataApp({super.key});
-
+class PlotDataApp extends StatelessWidget {
   @override
-  _PlotDataAppState createState() => _PlotDataAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PlotDataPage(),
+    );
+  }
 }
 
-class _PlotDataAppState extends State<PlotDataApp> {
+class PlotDataPage extends StatefulWidget {
+  @override
+  _PlotDataState createState() => _PlotDataState();
+}
+
+class _PlotDataState extends State<PlotDataPage> {
   DateTime? selectedDate;
 
-  void _onDateSelected(DateTime date) {
-    setState(() {
-      selectedDate = date;
-    });
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("File Operations"),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.calendar_today),
-              onPressed: () {
-                // 日付を選択
-              },
-            )
-          ],
-        ),
-        body: Row(
-          children: [
-            Expanded(
-              child: FileUploaderScreen(),
-            ),
-            Expanded(
-              child: FileDownloaderScreen(selectedDate: selectedDate),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.calendar_today),
+            onPressed: () {
+              _selectDate(context);
+            },
+          ),
+        ],
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            child: FileUploaderScreen(),
+          ),
+          Expanded(
+            child: FileDownloaderScreen(selectedDate: selectedDate),
+          ),
+        ],
       ),
     );
   }
