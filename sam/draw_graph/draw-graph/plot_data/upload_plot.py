e62c3f1028f6f02b12b77b6b374d203ffb185ed6
sota1111 <sota.moro@gmail.com>
2023-10-03 13:36:01 +0900

    リファクタリング

--- sam/draw_graph/draw-graph/plot_data/upload_plot.py
+++ sam/draw_graph/draw-graph/plot_data/upload_plot.py
@@ -7,6 +7,19 @@ import matplotlib.pyplot as plt
 import os  # osライブラリを追加
 
 
+def plot_and_save(df, file_name):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(df['data_x'], df['data_y'], marker='o')
+    ax.set_xlabel('time')
+    ax.set_ylabel('data')
+    ax.grid(True)
+    print(f"Finish plot")
+    
+    png_file_name = os.path.splitext(file_name)[0] + '.png'
+    fig.savefig(f'/tmp/{png_file_name}')
+    
+    return png_file_name
+
 def lambda_handler(event, context):
     print(f"call upload")
     try:
@@ -15,44 +28,26 @@ def lambda_handler(event, context):
         file_data_b64 = body.get('file_data', '')
         file_data = base64.b64decode(file_data_b64)
         file_name = body.get('file_name', 'unknown.csv')
-        file_name = body.get('experiment_date', '2023-10-3')
-        file_name = body.get('experiment_number', '0')
-        file_name = body.get('message', 'hello')
-
+        experiment_date = body.get('experiment_date', '2023-10-3')
+        experiment_number = body.get('experiment_number', '0')
+        message = body.get('message', 'hello')
         print(f"call upload:{file_name}")
 
         # S3クライアントを初期化
         s3 = boto3.client('s3')
 
-        # S3にファイルをアップロード
+        # S3にcsvファイルをアップロード
         bucket_name = 'log-robot-data'
         s3.put_object(Body=file_data, Bucket=bucket_name, Key=file_name)
-
         print(f"uploaded csv:{file_name}")
 
+        # プロット
         df = pd.read_csv(io.BytesIO(file_data))
-        print(df.head())
-
-
-        # データをプロット
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.plot(df['data_x'], df['data_y'], marker='o')
-        ax.set_xlabel('time')
-        ax.set_ylabel('data')
-        ax.grid(True)
-
+        png_file_name = plot_and_save(df, file_name)
         print(f"finish plot")
 
-        # プロットをバイトデータとして保存
-        png_file_name = os.path.splitext(file_name)[0] + '.png'
-        fig.savefig(f'/tmp/{png_file_name}')
-        
-        client = boto3.client('s3')
-        client.upload_file(
-            f'/tmp/{png_file_name}',
-            bucket_name,
-            png_file_name
-        )
+        # S3にpngファイルをアップロード
+        s3.upload_file(f'/tmp/{png_file_name}', bucket_name, png_file_name)
 
         return {
             "statusCode": 200,
