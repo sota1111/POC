c66067fe5f63f17ff6a08878f926130a835b1fa8
sota1111 <sota.moro@gmail.com>
2023-10-05 17:28:22 +0900

    リファクタリング

--- sam/draw_graph/draw-graph/plot_data/download_plot.py
+++ sam/draw_graph/draw-graph/plot_data/download_plot.py
@@ -3,57 +3,57 @@ import boto3
 import base64
 import os
 
+def get_experiment_data_from_dynamodb(experiment_date, experiment_number):
+    dynamodb = boto3.resource('dynamodb')
+    db = dynamodb.Table('experiment')
+    db_response = db.get_item(
+        Key={
+            'Date': experiment_date,
+            'OrderID': experiment_number
+        }
+    )
+    return db_response.get('Item', {})
+
+def fetch_s3_object(bucket_name, file_key):
+    s3 = boto3.client('s3')
+    return s3.get_object(Bucket=bucket_name, Key=file_key)['Body'].read()
+
+def to_base64_encoded(content):
+    return base64.b64encode(content).decode('utf-8')
+
+def get_content_type(file_path):
+    file_extension = os.path.splitext(file_path)[1][1:]
+    return f"image/{file_extension}"
+
 def lambda_handler(event, context):
-    print(f"call download_plot")
     try:
-        # DynamoDBの設定
-        dynamodb = boto3.resource('dynamodb')
-        db = dynamodb.Table('experiment')
-        
-        # データをJSONとしてパース
+        # Parse request data
         body = json.loads(event['body'])
         experiment_date = body.get('experiment_date', '2023-10-3')
-        print(f"experiment_date:{experiment_date}")
         experiment_number = int(body.get('experiment_number', '0'))
-        print(f"experiment_number:{experiment_number}")
-
-        # DynamoDBから必要なデータを取得
-        db_response = db.get_item(
-            Key={
-                'Date': experiment_date,
-                'OrderID': experiment_number
-            }
-        )
-        item = db_response.get('Item', {})
-        print(f"db item:{item}")
+        
+        # Fetch experiment data from DynamoDB
+        item = get_experiment_data_from_dynamodb(experiment_date, experiment_number)
         log_png = item.get('log_png', '')
         trajectory = item.get('trajectory', '')
         continuous = item.get('continuous', '')
-
-        s3 = boto3.client('s3')
-        bucket_name = 'log-robot-data'  # バケット名を指定
-        # Construct the file key dynamically based on experiment_date and experiment_number
-        file_key_log = f"{experiment_date}/{experiment_number}/{log_png}"
-        file_key_tra = f"{experiment_date}/{experiment_number}/{trajectory}"
-        file_key_con = f"{experiment_date}/{experiment_number}/{continuous}"
-        s3_object_log = s3.get_object(Bucket=bucket_name, Key=file_key_log)
-        s3_object_tra = s3.get_object(Bucket=bucket_name, Key=file_key_tra)
-        s3_object_con= s3.get_object(Bucket=bucket_name, Key=file_key_con)
-        content_log = s3_object_log['Body'].read()
-        content_tra = s3_object_tra['Body'].read()
-        content_con = s3_object_con['Body'].read()
         
-        # バイトデータをBase64エンコーディング
-        base64_encoded_log = base64.b64encode(content_log).decode('utf-8')
-        base64_encoded_tra = base64.b64encode(content_tra).decode('utf-8')
-        base64_encoded_con = base64.b64encode(content_con).decode('utf-8')
+        # Fetch files from S3
+        bucket_name = 'log-robot-data'
+        content_log = fetch_s3_object(bucket_name, f"{experiment_date}/{experiment_number}/{log_png}")
+        content_tra = fetch_s3_object(bucket_name, f"{experiment_date}/{experiment_number}/{trajectory}")
+        content_con = fetch_s3_object(bucket_name, f"{experiment_date}/{experiment_number}/{continuous}")
 
-        file_extension_tra = os.path.splitext(trajectory)[1][1:]
-        content_type_tra = f"image/{file_extension_tra}" 
-        file_extension_con = os.path.splitext(continuous)[1][1:] 
-        content_type_con = f"image/{file_extension_con}" 
+        # Encode files to Base64
+        base64_encoded_log = to_base64_encoded(content_log)
+        base64_encoded_tra = to_base64_encoded(content_tra)
+        base64_encoded_con = to_base64_encoded(content_con)
         
-        # JSONレスポンスを作成
+        # Determine content types
+        content_type_tra = get_content_type(trajectory)
+        content_type_con = get_content_type(continuous)
+        
+        # Build response
         return {
             "statusCode": 200,
             "headers": {
@@ -71,7 +71,6 @@ def lambda_handler(event, context):
             }),
         }
     except Exception as e:
-        print(f"Exception: {str(e)}")
         return {
             "statusCode": 500,
             "headers": {
