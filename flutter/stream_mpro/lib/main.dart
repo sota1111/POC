456aa8719335454ce69a1729ebca71ac5fd5c077
sota1111 <sota.moro@gmail.com>
2023-10-10 16:20:27 +0900

    シークバーを動かす速さを設定

--- flutter/stream_mpro/lib/main.dart
+++ flutter/stream_mpro/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 void main() {
@@ -12,6 +13,7 @@ class StreamVideo extends StatefulWidget {
 
 class _StreamVideoState extends State<StreamVideo> {
   late VideoPlayerController _controller;
+  final FocusNode _focusNode = FocusNode();
 
   @override
   void initState() {
@@ -29,31 +31,44 @@ class _StreamVideoState extends State<StreamVideo> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: _controller.value.isInitialized
-            ? Stack(
-          children: [
-            AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Slider(
-                value: _controller.value.position.inMilliseconds.toDouble(),
-                min: 0.0,
-                max: _controller.value.duration.inMilliseconds.toDouble(),
-                onChanged: (double value) {
-                  setState(() {
-                    _controller.seekTo(Duration(milliseconds: value.toInt()));
-                  });
-                },
+        body: RawKeyboardListener(
+          focusNode: _focusNode,
+          autofocus: true,
+          onKey: (RawKeyEvent event) {
+            if (event is RawKeyDownEvent) {
+              if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+                _seekForward();
+              } else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+                _seekBackward();
+              }
+            }
+          },
+          child: _controller.value.isInitialized
+              ? Stack(
+            children: [
+              AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
               ),
-            ),
-          ],
-        )
-            : Center(child: CircularProgressIndicator()),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Slider(
+                  value: _controller.value.position.inMilliseconds.toDouble(),
+                  min: 0.0,
+                  max: _controller.value.duration.inMilliseconds.toDouble(),
+                  onChanged: (double value) {
+                    setState(() {
+                      _controller.seekTo(Duration(milliseconds: value.toInt()));
+                    });
+                  },
+                ),
+              ),
+            ],
+          )
+              : Center(child: CircularProgressIndicator()),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             setState(() {
@@ -70,9 +85,28 @@ class _StreamVideoState extends State<StreamVideo> {
     );
   }
 
+  void _seekForward() {
+    final newPosition = _controller.value.position + Duration(seconds: 1);  // 進む長さを5秒に設定
+    if (newPosition <= _controller.value.duration) {
+      _controller.seekTo(newPosition);
+    } else {
+      _controller.seekTo(_controller.value.duration);
+    }
+  }
+
+  void _seekBackward() {
+    final newPosition = _controller.value.position - Duration(seconds: 1);  // 戻る長さを5秒に設定
+    if (newPosition >= Duration.zero) {
+      _controller.seekTo(newPosition);
+    } else {
+      _controller.seekTo(Duration.zero);
+    }
+  }
+
   @override
   void dispose() {
     super.dispose();
     _controller.dispose();
+    _focusNode.dispose();
   }
 }
